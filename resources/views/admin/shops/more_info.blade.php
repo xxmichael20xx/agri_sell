95e12d3275704172632c36d18899e49045979b69
xxmichael20xx <mamaclaymichael20@gmail.com>
2022-08-13 21:03:13 +0800

    fix

--- resources/views/admin/shops/more_info.blade.php
+++ resources/views/admin/shops/more_info.blade.php
@@ -11,9 +11,7 @@
                 </div>
                 <div class="card-body">
                     <div class="author">
-                        <a href="">                 
-                            <h5 class="title text-center">{{ $shop->name }}</h5>
-                        </a>
+                        <h5 class="title text-center">{{ $shop->name }}</h5>
                     </div>
                     <p class="description text-center">
                         {{ $shop->description }}
@@ -24,13 +22,13 @@
                     <div class="button-container">
                         <div class="row">
                             <div class="col-lg-3 col-md-6 col-6 ml-center">
-                                <h5 class="text-center">{{ $order_count }}<br><small>Total orders</small></h5>
+                                <h5 class="text-center dark-highlight">{{ $order_count }}<br><small>Total orders</small></h5>
                             </div>
                             <div hidden class="col-lg-4 col-md-6 col-6 ml-auto mr-auto">
-                                <h5 class="text-center">{{ $shopAveRating }}<br><small>Average rating</small></h5>
+                                <h5 class="text-center dark-highlight">{{ $shopAveRating }}<br><small>Average rating</small></h5>
                             </div>
                             <div hidden class="col-lg-3 mr-auto">
-                                <h5 class="text-center">&#8369;{{ $total_sales }}<br><small>Total sales</small></h5>
+                                <h5 class="text-center dark-highlight">&#8369;{{ $total_sales }}<br><small>Total sales</small></h5>
                             </div>
                         </div>
                     </div>
@@ -56,7 +54,7 @@
                     <div class="row">
                         <div class="col-md-12 pr-1">
                             <div class="form-group">
-                                <label>Owner name</label>
+                                <label class="dark-highlight">Owner name</label>
                                 <text class="form-control border-0">{{ $shop->owner->name ?? 'not available'}} </text>
                             </div>
                         </div>
@@ -72,13 +70,13 @@
                     <div class="row">
                         <div class="col-md-6 pr-1">
                             <div class="form-group">
-                                <label>Email</label>
+                                <label class="dark-highlight">Email</label>
                                 <text class="form-control border-0">{{ $shop->owner->email ?? 'not available'}} </text>
                             </div>
                         </div>
                         <div class="col-md-6 pl-1">
                             <div class="form-group">
-                                <label>Mobile</label>
+                                <label class="dark-highlight">Mobile</label>
                                 <text class="form-control border-0">{{ $shop->owner->mobile ?? 'not available' }} </text>
                             </div>
                         </div>
@@ -86,7 +84,7 @@
                     <div class="row">
                         <div class="col-md-12">
                             <div class="form-group">
-                                <label>Birth day</label>
+                                <label class="dark-highlight">Birth day</label>
                                 <text class="form-control border-0">{{ $shop->owner->bday ?? 'not available'}} </text>
                             </div>
                         </div>
@@ -94,19 +92,19 @@
                     <div class="row">
                         <div class="col-md-4 pr-1">
                             <div class="form-group">
-                                <label>Province</label>
+                                <label class="dark-highlight">Province</label>
                                 <text class="form-control border-0">{{ $shop->owner->province ?? 'not available'}} </text>
                             </div>
                         </div>
                         <div class="col-md-4 px-1">
                             <div class="form-group">
-                                <label>Town/City</label>
+                                <label class="dark-highlight">Town/City</label>
                                 <text class="form-control border-0">{{ $shop->owner->town ?? 'not available'}} </text>
                             </div>
                         </div>
                         <div class="col-md-4 pl-1">
                             <div class="form-group">
-                                <label>Barangay</label>
+                                <label class="dark-highlight">Barangay</label>
                                 <text class="form-control border-0">{{ $shop->owner->barangay ?? 'not available' }} </text>
                             </div>
                         </div>
@@ -123,10 +121,9 @@
                 @endphp
                     <h5 class="title">Payment details</h5>
                     <div class="row">
-                        <div class="col-md-12 pr-1">
+                        <div class="col-md-8 pr-1">
                             @if (isset($payment_details_obj->payment_proof))
-                            <img src="{{asset('storage/'.$payment_details_obj->payment_proof)}}" alt="Not available">
-
+                                <img src="{{asset('storage/'.$payment_details_obj->payment_proof)}}" alt="Not available">
                             @endif
                         </div>
                     </div>
